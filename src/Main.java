e22805f724c641230e63cdc69478c77c19f82c42
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-18 11:45:32 +0100

    comments and cleaning

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,25 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException{
             Scanner console = new Scanner(System.in);
-
-
+            
             System.out.print("file #1 name? ");
-            Scanner input1 = new Scanner(new File(console.nextLine()));
+            Scanner input1 = new Scanner(new File(console.nextLine())); //ask for file name "hamlet.txt"
             System.out.print("file #2 name? ");
-            Scanner input2 = new Scanner(new File(console.nextLine()));
+            Scanner input2 = new Scanner(new File(console.nextLine())); //ask for file name "mobydick.txt"
             System.out.println();
 
-            ArrayList<String> list1 = getWords(input1);
-            ArrayList<String> list2 = getWords(input2);
-
-
+            ArrayList<String> list1 = getWords(input1); //Takes the input and reads the file with that name "hamlet.txt"
+            ArrayList<String> list2 = getWords(input2); //Takes the input and reads the file with that name "mobydick.txt"
         }
+
         public static ArrayList<String> getWords(Scanner input) {
             // read all words and sort
            Set<String> words = new TreeSet<>();
             while (input.hasNext()) {
-                String next = input.next().toLowerCase();
+                String next = input.next().toLowerCase(); // Makes all words lowercase to match
                 words.add(next);
             }
-            System.out.println(words.size());
-            return new ArrayList<>(words);
+            System.out.println(words.size()); //prints the size of the set words that cointains uniq words
+            return new ArrayList<>(words); //Returns a new arratlist with the words found in the set
         }
     }
